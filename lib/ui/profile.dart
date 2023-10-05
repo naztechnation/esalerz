d6aa8d90fc5abb84703ae3e6e9c0be1bc682eaf9
Devchuks98 <whizpherairmy@gmail.com>
2023-10-05 13:34:48 +0100

    implemented a grid and list style for the similar ads section, under the userservice_info page and on the homepages

--- lib/ui/profile.dart
+++ lib/ui/profile.dart
@@ -1,3 +1,4 @@
+import 'package:esalerz/res/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
@@ -5,6 +6,24 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.cardColor,
+        elevation: 0,
+        title: const Text(
+          'Hello, Samson',
+          style: TextStyle(
+              fontSize: 15, color: Colors.black, fontWeight: FontWeight.w900),
+        ),
+        leading: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: const Center(
+            child: Icon(Icons.menu, color: AppColors.lightPrimary),
+          ),
+        ),
+      ),
+    );
   }
 }
